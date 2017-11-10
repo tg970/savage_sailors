0d256c7b335c3c70ca17f803868ce86ac67362f7
tg970 <tgoodman@outlook.com>
2017-11-10 13:52:52 -0600

    placing boats, no html update yet tho

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 //console.log($);
 
 // === GLOBAL VARIABLES ===
-let userBoard = [];
-let enemyBoard = [];
+let userBoats = [];
+let enemyBoats = [];
+let boats = 3;
 let size = 4;
+let availBoats = [['Tender', 1],['Fishing',2],['Cat',3]]
 
 // === BOARD CONSTRUCTION ===
 const buildBoards = (size) => {
@@ -13,7 +15,7 @@ const buildBoards = (size) => {
       for (let j = 0; j < size; j++) {
          let $sq = $('<div>').addClass('heroSq').attr('id',`${j}`)
          $sq.addClass(`${i}`)
-         //$sq.on('click',boatPlacement)
+         $sq.on('click',boatPlacement)
          $container.append($sq)
       };
    $('.hero').append($container)
@@ -24,18 +26,58 @@ const buildBoards = (size) => {
       for (let j = 0; j < size; j++) {
          let $sq = $('<div>').addClass('enemySq').attr('id',`${j}`)
          $sq.addClass(`${i}`)
-         //$sq.on('click',userShot)
+         $sq.on('click',userShot)
          $container.append($sq)
       };
    $('.enemy').append($container)
    }
 }
 
+// === CLICK HANDERLERS ===
 
+const userShot = (event) => {
+   console.log('click enemy square');
+}
 
-// === WINDOW ON LOAD/PAGE READY
+const boatPlacement = (event) => {
+   if (boats > 0) {
+      let row = $(event.currentTarget).attr('class').split(' ')[1];
+      let col = $(event.currentTarget).attr('id');
+      userBoats.unshift(new Boat (availBoats[0][0],availBoats[0][1]));
+      availBoats.shift();
+      boats--
+      console.log(userBoats, availBoats, boats);
+   }
+   if (boats == 1) {
+
+   }
+}
+
+// === LOGIC LAYER ===
+
+class Boat {
+   constructor(name, length) {
+      this.name = name,
+      this.length = length,
+      this.position = {},
+      this.hit = []
+   }
+   posBuild(event) {
+      for (let i = 0; i < this.length; i++) {
+         //this.position[i] = event click handler + i
+         //this.hit.push(false)
+      }
+   }
+}
+
+const startGame = () => {
+   buildBoards(size);
+   //alert('Click the square where you want to place your boat');
+}
+
+// === WINDOW ON LOAD/PAGE READY ===
 $(() => {
 
-buildBoards(size)
+startGame();
 
 })
