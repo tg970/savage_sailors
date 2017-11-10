40240b31348415a972c7ef57d2e4c137320886e4
tg970 <tgoodman@outlook.com>
2017-11-10 12:13:00 -0600

    boards constructed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
 //console.log($);
+
+// === GLOBAL VARIABLES ===
+let userBoard = [];
+let enemyBoard = [];
+let size = 4;
+
+// === BOARD CONSTRUCTION ===
+const buildBoards = (size) => {
+   // Build User Board
+   for (let i = 0; i < size; i++) {
+      const $container = $('<div>').addClass('row')
+      for (let j = 0; j < size; j++) {
+         let $sq = $('<div>').addClass('heroSq').attr('id',`${j}`)
+         $sq.addClass(`${i}`)
+         //$sq.on('click',boatPlacement)
+         $container.append($sq)
+      };
+   $('.hero').append($container)
+   }
+   //Build Enemy Board
+   for (let i = 0; i < size; i++) {
+      const $container = $('<div>').addClass('row')
+      for (let j = 0; j < size; j++) {
+         let $sq = $('<div>').addClass('enemySq').attr('id',`${j}`)
+         $sq.addClass(`${i}`)
+         //$sq.on('click',userShot)
+         $container.append($sq)
+      };
+   $('.enemy').append($container)
+   }
+}
+
+
+
+// === WINDOW ON LOAD/PAGE READY
+$(() => {
+
+buildBoards(size)
+
+})
